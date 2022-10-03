6fa85e237182b048814bcc021a249ee462ccbfb3
novianto22 <99942803+novianto22@users.noreply.github.com>
2022-10-03 17:30:47 +0700

    On auth session depedencies

--- src/hooks/useAuthStateChange.tsx
+++ src/hooks/useAuthStateChange.tsx
@@ -24,8 +24,7 @@ const useAuthStateChange = () => {
     return () => {
       authListener?.unsubscribe();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [setUser, session]);
 
   return { user, session, loading };
 };
